418bde83fc62dde74518138b88f0588f3a8ecb47
edjunma <edjunma@gmail.com>
2019-11-23 03:49:03 -0500

    setup backdrop fix

--- src/components/Root/Springboard/OpenFolderBackdrop/OpenFolderBackdrop.js
+++ src/components/Root/Springboard/OpenFolderBackdrop/OpenFolderBackdrop.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { animated, useSpring } from 'react-spring';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-	backdrop-filter: blur(20px);
+const Wrapper = styled(animated.div)`
 	height: 100%;
 	left: 0;
 	position: absolute;
+	pointer-events: none;
 	top: 0;
 	width: 100%;
 
@@ -14,8 +15,25 @@ const Wrapper = styled.div`
 	}
 `;
 
-const OpenFolderBackdrop = () => {
-	return isVisible ? <Wrapper /> : null;
+const MAXIMUM_BLUR = 20;
+const MINIMUM_OPACITY_AMOUNT_ON_SHOW = 0.1;
+const MINIMUM_OPACITY_AMOUNT_ON_HIDE = 0.9;
+
+const OpenFolderBackdrop = ({ isVisible }) => {
+	const spring = useSpring({ openAmount: isVisible ? 1 : 0 });
+
+	const style = {
+		opacity: spring.openAmount.interpolate(openAmount => `blur(${openAmount * MAXIMUM_BLUR}px)`),
+		opacity: spring.openAmount,
+		pointerEvents: spring.openAmount.interpolate(openAmount => {
+			return (isVisible && openAmount >= MINIMUM_OPACITY_AMOUNT_ON_SHOW) ||
+				(!isVisible && openAmount >= MINIMUM_OPACITY_AMOUNT_ON_HIDE)
+				? 'auto'
+				: 'none';
+		})
+	};
+
+	return <Wrapper style={style} />;
 };
 
 export default OpenFolderBackdrop;
